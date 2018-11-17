f3d7172aac7721a9bbd6bc4ac715d88b63454f22
UmeNoAto <frank.gong.1006@hotmail.com>
2018-11-17 18:54:19 -0500

    Completed model for client model class

--- src/main/java/com/boundless/ftms/model/Client.java
+++ src/main/java/com/boundless/ftms/model/Client.java
@@ -1,4 +1,48 @@
 package com.boundless.ftms.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Client {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "clients_id")
+    private int clientsID;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    public Client() {
+    }
+
+    public int getClientsID() {
+        return clientsID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setClientsID(int clientsID) {
+        this.clientsID = clientsID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
