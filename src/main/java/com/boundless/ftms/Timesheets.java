33a9b1b3247d893a642f7f541145fa1dd28da398
UmeNoAto <frank.gong.1006@hotmail.com>
2018-11-17 17:31:54 -0500

    Added several columns in the class TImesheets

--- src/main/java/com/boundless/ftms/Timesheets.java
+++ src/main/java/com/boundless/ftms/Timesheets.java
@@ -5,6 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import java.sql.*;
+
 @Entity
 public class Timesheets {
 
@@ -12,6 +14,18 @@ public class Timesheets {
     @GeneratedValue
     @Column(name = "timesheet_id")
     private String name;
+
+    @Column(name = "start_time")
+    private Timestamp startTime = null;
+
+    @Column(name = "end_time")
+    private Timestamp endTime = null;
+
+    @Column(name = "worker_id")
+    private int workerID;
+
+    @Column(name = "job_id")
+    private int jobID;
 
     public String getName() {
         return name;
