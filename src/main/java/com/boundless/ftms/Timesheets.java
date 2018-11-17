accd981e292682b7a23254a491ce5395f9a73c02
UmeNoAto <frank.gong.1006@hotmail.com>
2018-11-17 18:18:14 -0500

    Getters and setters of all columns

--- src/main/java/com/boundless/ftms/Timesheets.java
+++ src/main/java/com/boundless/ftms/Timesheets.java
@@ -33,5 +33,59 @@ public class Timesheets {
     @Column(name = "time_approved")
     private Timestamp timeApproved = null;
 
+    public String getName() {
+        return name;
+    }
 
+    public int getWorkerID() {
+        return workerID;
+    }
+
+    public int getJobID() {
+        return jobID;
+    }
+
+    public Timestamp getTimeSumbitted() {
+        return timeSumbitted;
+    }
+
+    public String getApprovalStatus() {
+        return approvalStatus;
+    }
+
+    public String getRejectionReason() {
+        return rejectionReason;
+    }
+
+    public Timestamp getTimeApproved() {
+        return timeApproved;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setWorkerID(int workerID) {
+        this.workerID = workerID;
+    }
+
+    public void setJobID(int jobID) {
+        this.jobID = jobID;
+    }
+
+    public void setTimeSumbitted(Timestamp timeSumbitted) {
+        this.timeSumbitted = timeSumbitted;
+    }
+
+    public void setApprovalStatus(String approvalStatus) {
+        this.approvalStatus = approvalStatus;
+    }
+
+    public void setRejectionReason(String rejectionReason) {
+        this.rejectionReason = rejectionReason;
+    }
+
+    public void setTimeApproved(Timestamp timeApproved) {
+        this.timeApproved = timeApproved;
+    }
 }
