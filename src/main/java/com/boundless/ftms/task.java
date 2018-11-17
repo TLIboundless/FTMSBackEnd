a9943276447363eb09f3f82abd7c8b0faba0cbec
ruth.crasto <ruth.crasto@mail.utoronto.ca>
2018-11-17 13:30:43 -0500

    Modified Task class annotations to agree with DB implementation. Added Part class.

--- src/main/java/com/boundless/ftms/task.java
+++ src/main/java/com/boundless/ftms/task.java
@@ -4,15 +4,54 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.util.*;
+import java.sql.*;
 
 @Entity
-public class task {
+public class Task {
 
     @Id
     @GeneratedValue
     @Column(name = "name")
     private String name;
 
+    @Column(name = "startTime")
+    private Timestamp startTime;
+
+    @Column(name = "endTime")
+    private Timestamp endTime = null;
+
+    @Column(name = "duration")
+    private double duration; // in minutes
+
+    @Column(name = "workerID")
+    private int workerID;
+
+    @Column(name = "jobID")
+    private int jobID;
 
 
+    public double getDuration() {
+        return duration;
+    }
+
+    public void setDuration(double duration) {
+        this.duration = duration;
+    }
+
+    public Timestamp getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Timestamp endTime) {
+        this.endTime = endTime;
+    }
+
+    public void setStartTime(Timestamp startTime) {
+        this.startTime = startTime;
+    }
+
+    public Timestamp getStartTime() {
+        return startTime;
+    }
 }
