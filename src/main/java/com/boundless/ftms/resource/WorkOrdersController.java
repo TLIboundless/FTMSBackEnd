0d0b846efd3cc21ec09f7a6adba6ace6589bf0db
Kshitij Shah <kshnik@gmail.com>
2018-11-18 15:56:40 -0500

    fixed work orders typo

--- src/main/java/com/boundless/ftms/resource/WorkOrdersController.java
+++ src/main/java/com/boundless/ftms/resource/WorkOrdersController.java
@@ -3,7 +3,7 @@ package com.boundless.ftms.resource;
 import java.util.List;
 
 import com.boundless.ftms.model.WorkOrders;
-import com.boundless.ftms.repository.WorkOrderRepository;
+import com.boundless.ftms.repository.WorkOrdersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.*;
@@ -13,16 +13,16 @@ import org.springframework.web.bind.annotation.*;
 public class WorkOrdersController {
 
     @Autowired
-    WorkOrderRepository workOrderRepository;
+    WorkOrdersRepository workOrdersRepository;
 
     @RequestMapping(value = "/list", method = RequestMethod.GET)
     private List<WorkOrders> getAll() {
-        return workOrderRepository.findAll();
+        return workOrdersRepository.findAll();
     }
 
     @RequestMapping(value = "/add", method = RequestMethod.POST)
     public String saveToDatabase(@RequestBody final WorkOrders workOrder){
-        workOrderRepository.save(workOrder);
+        workOrdersRepository.save(workOrder);
         return "work order added";
     }
 
