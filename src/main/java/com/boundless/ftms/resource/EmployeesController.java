f8bb5fa39d9b02493f836cdcd1552368581c1475
Ben Prystawski <benpry@users.noreply.github.com>
2018-11-17 19:45:30 -0500

    replaced 'Worker' with 'Employee' to match table name

---
+++ src/main/java/com/boundless/ftms/resource/EmployeesController.java
@@ -0,0 +1,29 @@
+package com.boundless.ftms.resource;
+
+import java.util.List;
+
+import com.boundless.ftms.model.Employees;
+import com.boundless.ftms.repository.EmployeesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/rest/worker")
+public class WorkerController {
+    @Autowired
+    EmployeesRepository employeesRepository;
+
+    @GetMapping(value = "/all")
+    private List<Employees> getAll() {
+        return employeesRepository.findAll();
+    }
+
+    @PostMapping(value = "/insert")
+    public List<Employees> saveToDatabase(@RequestBody final Employees employees) {
+        employeesRepository.save(employees);
+        return employeesRepository.findAll();
+    }
+
+}
+
+
