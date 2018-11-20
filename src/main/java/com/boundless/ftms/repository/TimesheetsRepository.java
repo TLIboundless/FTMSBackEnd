49a0a3f16b4534894f607d1059ae42193bb4570b
ruth.crasto <ruth.crasto@mail.utoronto.ca>
2018-11-20 18:31:18 -0500

    Added a SQL query

--- src/main/java/com/boundless/ftms/repository/TimesheetsRepository.java
+++ src/main/java/com/boundless/ftms/repository/TimesheetsRepository.java
@@ -1,5 +1,20 @@
 package com.boundless.ftms.repository;
+
 import com.boundless.ftms.model.Timesheets;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.*;
+import java.util.*;
+
+
 public interface TimesheetsRepository extends JpaRepository<Timesheets, Integer>{
+
+    // SQL queries
+    public static final String FIND_TIMSHEETS_FROM_JOB = "SELECT * FROM timesheet WHERE job_id = ?1";
+
+
+    @Query(value = FIND_TIMSHEETS_FROM_JOB, nativeQuery = true)
+    public List<Timesheets> findTimesheetsFromJob(int job_id);
+
+
+
+
 }
