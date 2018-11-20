408cbdf2d5686bf70412912daf8f35f8ce90c268
Ben Prystawski <benpry@users.noreply.github.com>
2018-11-19 19:44:07 -0500

    changed primary key for jobs entity to job_id

--- src/main/java/com/boundless/ftms/model/Jobs.java
+++ src/main/java/com/boundless/ftms/model/Jobs.java
@@ -4,8 +4,11 @@ import javax.persistence.*;
 
 @Entity
 public class Jobs {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "job_id")
+    private int job_id;
     @Column (name = "worker_id")
     private int worker_id;
     @Column (name = "supervisor_id")
@@ -20,6 +23,14 @@ public class Jobs {
         this.workorder_id = workorder_id;
         this.supervisor_id = supervisor_id;
     }
+    public int getJob_id() {
+        return job_id;
+    }
+
+    public void setJob_id(int job_id) {
+        this.job_id = job_id;
+    }
+
     public int getWorker_id() {
         return worker_id;
     }
