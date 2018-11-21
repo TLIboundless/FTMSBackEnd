73493fdcd92c7c5de835e755537f454db92d90b2
ruth.crasto <ruth.crasto@mail.utoronto.ca>
2018-11-20 22:37:05 -0500

    Added a "delete" SQL command.

--- src/main/java/com/boundless/ftms/repository/TaskRepository.java
+++ src/main/java/com/boundless/ftms/repository/TaskRepository.java
@@ -12,9 +12,13 @@ public interface TaskRepository extends JpaRepository<Task, Integer> {
 
     // SQL queries
     public static final String FIND_TASKS_FROM_JOB = "SELECT * FROM tasks WHERE job_id = ?1";
+    public static final String DELETE_TASK_BY_ID = "DELETE FROM tasks WHERE task_id = ?1";
 
     @Query(value = FIND_TASKS_FROM_JOB, nativeQuery = true)
     public List<Timesheets> findTasksFromJob(int job_id);
+
+    @Query(value = DELETE_TASK_BY_ID, nativeQuery = true)
+    public void deleteTaskById(int task_id);
 
 
 }
