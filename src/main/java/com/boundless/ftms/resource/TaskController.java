51d47e984e573f113b3d6beb7da6a3716612f830
Kshitij Shah <kshnik@gmail.com>
2018-11-21 18:22:57 -0500

    Added way to update tasks name, start, end times.

--- src/main/java/com/boundless/ftms/resource/TaskController.java
+++ src/main/java/com/boundless/ftms/resource/TaskController.java
@@ -1,9 +1,13 @@
 package com.boundless.ftms.resource;
 
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import com.boundless.ftms.model.Task;
 import com.boundless.ftms.repository.TaskRepository;
+import org.omg.PortableServer.REQUEST_PROCESSING_POLICY_ID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,5 +34,22 @@ public class TaskController {
         return taskRepository.findTasksFromJob(jobId);
     }
 
+    @RequestMapping(value = "/overwrite", method = RequestMethod.POST)
+    public void overwriteExistingTask(@RequestParam String id,
+                                      @RequestParam String name,
+                                      @RequestParam String start,
+                                      @RequestParam String end) {
+        Task task = taskRepository.getOne(Integer.parseInt(id));
+        if (!name.equals("")) {
+            task.setName(name);
+        }
+        if (!start.equals("")) {
+            task.setStartTime(Timestamp.valueOf(start));
+        }
+        if (!end.equals("")) {
+            task.setEndTime(Timestamp.valueOf(end));
+        }
+        taskRepository.save(task);
+    }
 
 }
