3135eb88d2c84cba265f6de6503df5cee8e963ba
ruth.crasto <ruth.crasto@mail.utoronto.ca>
2018-11-21 23:19:05 -0500

    Attempt to fix a bug with SQL query.

--- src/main/java/com/boundless/ftms/repository/TimesheetsRepository.java
+++ src/main/java/com/boundless/ftms/repository/TimesheetsRepository.java
@@ -10,6 +10,13 @@ public interface TimesheetsRepository extends JpaRepository<Timesheets, Integer>
     // SQL queries
     public static final String FIND_TIMSHEETS_FROM_JOB = "SELECT * FROM timesheets WHERE job_id = ?1";
 
+    static final String FIND_PENDING_TIMESHEETS_FROM_JOBID = "SELECT * FROM timesheets WHERE approval_status = 'Pending' " +
+            "AND job_id = ?1";
+
     @Query(value = FIND_TIMSHEETS_FROM_JOB, nativeQuery = true)
     public List<Timesheets> findTimesheetsFromJob(int job_id);
+
+    @Query(value = FIND_PENDING_TIMESHEETS_FROM_JOBID, nativeQuery = true)
+    List<Timesheets> findPendingTimeSheetsFromJobId(int job_id);
+
 }
