1562e32ebccb7c64022ff344d28f8d2ceb4bfdd6
ruth.crasto <ruth.crasto@mail.utoronto.ca>
2018-11-21 23:08:22 -0500

    Fixed SQL command.

--- src/main/java/com/boundless/ftms/repository/JobsRepository.java
+++ src/main/java/com/boundless/ftms/repository/JobsRepository.java
@@ -15,7 +15,7 @@ public interface JobsRepository extends JpaRepository<Jobs, Integer>{
 
     String FIND_JOBS_FROM_WORKORDER_ID = "SELECT * FROM jobs WHERE workorder_id = ?1";
 
-    String FIND_PENDING_TIMESHEETS_FROM_JOBID = "SELECT * FROM timesheets WHERE approval_status = Pending " +
+    String FIND_PENDING_TIMESHEETS_FROM_JOBID = "SELECT * FROM timesheets WHERE approval_status = 'Pending' " +
             "AND job_id = ?1";
 
     @Query(value = FIND_JOBS_FROM_WORKER, nativeQuery = true)
