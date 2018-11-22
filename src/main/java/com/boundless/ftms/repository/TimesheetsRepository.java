702c3b3b1b53a8dcce670891928c0cec15a64f2f
asleshapokhrel <aslesha.pokhrel@mail.utoronto.ca>
2018-11-21 21:42:35 -0500

    Added queries in TimeSheetRepo

--- src/main/java/com/boundless/ftms/repository/TimesheetsRepository.java
+++ src/main/java/com/boundless/ftms/repository/TimesheetsRepository.java
@@ -10,10 +10,20 @@ public interface TimesheetsRepository extends JpaRepository<Timesheets, Integer>
     // SQL queries
     public static final String FIND_TIMSHEETS_FROM_JOB = "SELECT * FROM timesheets WHERE job_id = ?1";
 
+    String FIND_JOBID_FROM_WORKORDERID = "SELECT job_id FROM jobs WHERE workorder_id = ?1";
+
+    String FIND_PENDING_TIMESHEETS_FROM_JOBID = "SELECT * FROM timesheets WHERE approval_status = Pending " +
+            "AND job_id = ?1";
+
 
     @Query(value = FIND_TIMSHEETS_FROM_JOB, nativeQuery = true)
     public List<Timesheets> findTimesheetsFromJob(int job_id);
 
+    @Query(value = FIND_JOBID_FROM_WORKORDERID, nativeQuery = true)
+    List<Integer> findJobIdFromWorkOrderId(int workOrder_id);
+
+    @Query(value = FIND_PENDING_TIMESHEETS_FROM_JOBID, nativeQuery = true)
+    List<Timesheets> findPendingTimeSheetsFromJobId(int job_id);
 
 
 }
